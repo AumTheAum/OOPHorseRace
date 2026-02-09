a72fd8cadf9cd14021b3b40332d716dc6ad1813b
AumTheAum <littledotstuddo0@gmail.com>
2026-02-09 08:04:55 +0000

    my message

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "horse.h"
+
+void testHorse();
+
+int main(){
+	std::cout << "OOP Horse Race!!!" << std::endl;
+	testHorse();
+	
+	return 0;
+}
+
+void testHorse(){
+	Horse h;
+	h.init(1, 15);
+	bool keepGoing = true;
+	while (keepGoing){
+		h.advance();
+		h.printLane();
+		if (h.isWinner()){
+			keepGoing = false;
+		}
+	}
+}
